571e21af8798e757c1c354400d61571c6706d6cb
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-11-01 16:10:04 -0300

    chore: add custom app with main layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app'
+import { MainLayout } from '../layouts/MainLayout';
+
+import '../styles/global.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
+}

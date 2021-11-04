1a1c8b9bcf5eac7b8cdd3ddacf798f6914ce610c
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-11-04 13:19:06 -0300

    chore(MainLayout): wrap children with AuthenticationProvider

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,3 +1,5 @@
+import { AuthenticationProvider } from '../../context/Authentication';
+
 import { AboutProjectSection } from './components/AboutProjectSection';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
@@ -15,7 +17,9 @@ export function MainLayout({ children }: MainLayoutProps) {
 
       <div className={styles.pagesWrapper}>
         <section>
-          {children}
+          <AuthenticationProvider>
+           {children}
+          </AuthenticationProvider>
         </section>
 
         <AboutProjectSection />

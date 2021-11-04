db1fcd60e85dd56332bd84701e5a7b4aa4b06721
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-11-04 11:58:00 -0300

    chore: add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import { ButtonHTMLAttributes } from "react";
+
+import styles from './styles.module.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: string;
+  showLoader?: boolean;
+};
+
+export function Button({ children, showLoader = false, ...rest }: ButtonProps) {
+  return (
+    <button className={styles.container} {...rest}>
+      <span className={`${showLoader ? styles.hidden : ''}`}>{children}</span>
+      {showLoader && <div className={styles.spinner}></div>}
+    </button>
+  );
+}

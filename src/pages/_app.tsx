effdeaa206c58b92b3639643db3f5af80a0219c8
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-11-04 13:23:53 -0300

    chore: add profile info to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+
 import { MainLayout } from '../layouts/MainLayout';
 
 import '../styles/global.scss';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,92 @@
-import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+
+import { getAccessTokenFromCookies, getProfileDataFromCookies } from '../utils/authentication.utils';
+
+import { useAuthentication } from '../context/Authentication';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-const Home: NextPage = () => {
+// TODO: Get access token from cookies and set to axios auth api service instance
+
+export default function HomePage() {
+  const router = useRouter();
+  const { signOut, loadProfile, profile } = useAuthentication();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSignOut() {
+    router.replace('/welcome-back');
+    signOut();
+  }
+
+  useEffect(() => {
+    async function loadProfileData() {
+      setIsLoading(true);
+
+      try {
+        await loadProfile();
+      } catch (error) {
+        // TODO: Tratar o erro
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadProfileData();
+  }, [loadProfile]);
+
+  if (isLoading) {
+    return (
+      <h3>Loading profile...</h3>
+    );
+  }
+
   return (
     <div className={commonStyles.pageContainer}>
-      <h1>Hello John</h1>
+      <h1>Hello { profile?.name }</h1>
 
       <div className={styles.infoContainer}>
         <small>Name</small>
-        <span>John Doe</span>
+        <span>{profile?.name}</span>
       </div>
 
       <div className={styles.infoContainer}>
         <small>Email</small>
-        <span>john.doe@email.com</span>
+        <span>{profile?.email}</span>
       </div>
+
+      <button onClick={handleSignOut}>Sair</button>
     </div>
   );
 }
 
-export default Home;
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const accessToken = getAccessTokenFromCookies(req.cookies);
+
+  if (!accessToken) {
+    const profile = getProfileDataFromCookies(req.cookies);
+
+    if (profile) {
+      return {
+        redirect: {
+          destination: '/welcome-back',
+          permanent: false,
+        }
+      };
+    }
+
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {},
+  };
+}

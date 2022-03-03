14289ae890c846a0e294ecfed9de4f3cd0bcce64
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-03-03 14:33:00 -0300

    feat(Button): udpate styles

---
+++ src/components/form/Button/styles.ts
@@ -0,0 +1,44 @@
+import { styled } from "@styles/stitches.config";
+
+export namespace FormButtonStyles {
+  export const Button = styled('button', {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    minWidth: '100px',
+    height: '42px',
+
+    paddingHorizontal: '$normal',
+
+    border: 'none',
+    borderRadius: '$normal',
+
+    fontSize: '$normal',
+    lineHeight: '$normal',
+    fontWeight: 'bold',
+    letterSpacing: '0.5px',
+
+    cursor: 'pointer',
+
+    backgroundColor: '$primaryElementBackground',
+    color: '$primarySolid',
+
+    outlineWidth: 2,
+    outlineStyle: 'solid',
+    outlineColor: 'transparent',
+
+    transitionAnimation: 'color, background-color, outline-color',
+
+    '&:hover': {
+      backgroundColor: '$primaryHoveredElementBackground',
+      color: '$primarySolid',
+    },
+
+    '&:focus-visible': {
+      backgroundColor: '$primaryHoveredElementBackground',
+      color: '$primaryHoveredSolid',
+      outlineColor: '$primaryHoveredElementBorder',
+    },
+  });
+}

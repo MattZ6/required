ce0aa545a41298c3378eda6f106f41c2af378ba0
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-03-14 14:48:05 -0300

    chore(SEO): add icon and image

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -6,12 +6,19 @@ import { useTranslation } from '@hooks/useTranslation';
 
 import { theme, darkTheme } from '@styles/stitches.config';
 
+type IconProps = {
+  path: string;
+  type: string;
+};
+
 type Props = {
   title?: string;
   description?: string;
+  icon?: IconProps;
+  image?: string;
 };
 
-export function SEO({ title, description }: Props) {
+export function SEO({ title, description, image, icon }: Props) {
   const router = useRouter();
   const { theme: currentTheme } = useTheme();
   const t = useTranslation('app');
@@ -31,7 +38,14 @@ export function SEO({ title, description }: Props) {
     <Head>
       <title>{pageTitle}</title>
       <meta name="description" content={description} />
-      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+      <link
+        rel="shortcut icon"
+        href={icon?.path ?? '/favicon.png'}
+        type={icon?.type ?? 'image/png'}
+      />
+
+      {image && <meta name="image" itemProp="image" content={image} />}
 
       <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
       <meta name="MobileOptimized" content="320" />
@@ -40,28 +54,37 @@ export function SEO({ title, description }: Props) {
       <meta name="msapplication-TileColor" content={color} />
       <meta name="referrer" content="no-referrer-when-downgrade" />
       <meta name="google" content="notranslate" />
-
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:locale" content={pageLocale} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={pageTitle} />
-      {/* <meta property="og:image" content={pageImage} /> */}
-      {/* <meta property="og:image:secure_url" content={pageImage} /> */}
-      {/* <meta property="og:image:alt" content="Thumbnail" /> */}
-      {/* <meta property="og:image:type" content="image/png" /> */}
-      {/* <meta property="og:image:width" content="1200" /> */}
-      {/* <meta property="og:image:height" content="630" /> */}
+
+      {image && (
+        <>
+          <meta property="og:image" content={image} />
+          <meta property="og:image:secure_url" content={image} />
+          <meta property="og:image:alt" content="Thumbnail" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+        </>
+      )}
 
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:card" content="summary_large_image" />
       {/* <meta name="twitter:site" content="" /> */}
       {/* <meta name="twitter:creator" content="" /> */}
-      {/* <meta name="twitter:image" content={pageImage} /> */}
-      {/* <meta name="twitter:image:src" content={pageImage} /> */}
-      {/* <meta name="twitter:image:alt" content="Thumbnail" /> */}
-      {/* <meta name="twitter:image:width" content="1200" /> */}
-      {/* <meta name="twitter:image:height" content="620" /> */}
+
+      {image && (
+        <>
+          <meta name="twitter:image" content={image} />
+          <meta name="twitter:image:src" content={image} />
+          <meta name="twitter:image:alt" content="Thumbnail" />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="620" />
+        </>
+      )}
     </Head>
   );
 }
--- src/pages/sign/in/index.tsx
+++ src/pages/sign/in/index.tsx
@@ -9,6 +9,7 @@ import {
 } from '@components/pages/components';
 import { CreateAccountLink, SignInForm } from '@components/pages/sign/in';
 import { CommonPageStyles as Styles } from '@components/pages/styles';
+import { SEO } from '@components/SEO';
 import { Text } from '@components/Text';
 
 export default function SignInPage() {
@@ -16,6 +17,13 @@ export default function SignInPage() {
 
   return (
     <Styles.Page>
+      <SEO
+        title={t('title')}
+        description={t('description')}
+        icon={{ path: '/favicon.png', type: 'image/png' }}
+        image="/assets/seo/pages/profile.png"
+      />
+
       <PageHeader title={t('title')} description={t('description')} />
 
       <SignInForm />

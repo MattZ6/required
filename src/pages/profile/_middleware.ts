ff49fc82a18e0abbe207e9f624b47c585352f9a8
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-11-04 11:44:52 -0300

    chore: delete all middlewares

--- src/pages/profile/_middleware.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from 'next/server';
-
-import { checkAuthentication } from '@utils/middlewares';
-
-export function middleware(req: NextRequest) {
-  return checkAuthentication(req);
-}
--- src/pages/welcome-back/_middleware.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from 'next/server';
-
-import { checkPreviousAuthentication } from '@utils/middlewares';
-
-export function middleware(req: NextRequest) {
-  return checkPreviousAuthentication(req);
-}

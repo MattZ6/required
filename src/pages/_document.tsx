bb48fee7b96b3fbb725962c23455529f3efb38f5
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-01-24 13:56:04 -0300

    chore(_document): set crossOrigin to anonymous

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class CustomDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"

7aab1095eed148d81e6e52697ba88971c340de2e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-05-02 17:16:37 -0300

    feat: add styles to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import '@styles/global.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { Footer } from '@components/Footer'
+import { Header } from '@components/Header'
+
+import styles from './layout.module.scss'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +24,11 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main className={styles.main}>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }

b713bbbe7e5d2c122d687de372d3f61357657d9b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-05-03 14:48:13 -0300

    feat(Field): add leadingIcon prop

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -6,14 +6,21 @@ import styles from './styles.module.scss'
 type FieldProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'id'> & {
   name: string
   label?: string
+  leadingIcon?: 'envelope-simple' | 'lock-simple'
   error?: FieldError
 }
 
-export function Field({ label, error, ...props }: FieldProps) {
+export function Field({ label, leadingIcon, error, ...props }: FieldProps) {
   return (
     <div className={styles.field}>
       {label && <label htmlFor={props.name}>{label}</label>}
-      <input id={props.name} {...props} />
+
+      <div className={styles.inputContainer}>
+        {leadingIcon && <i className={`ph ph-${leadingIcon}`}></i>}
+
+        <input id={props.name} {...props} />
+      </div>
+
       <div className={styles.errorContainer}>
         {error && <span>{error.message}</span>}
       </div>

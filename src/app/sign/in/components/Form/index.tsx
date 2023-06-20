449136d7b412e430be2eba11bdebf4913419fc0b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-06-20 15:53:43 -0300

    feat(Form): add sign in implementation

--- src/app/sign/in/components/Form/index.tsx
+++ src/app/sign/in/components/Form/index.tsx
@@ -4,6 +4,11 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useLayoutEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
+import { useSignIn } from '@hooks/useSignIn'
+
+import { parseError } from '@utils/parseRequestError'
+
+import { showAlert } from '@components/AlertDialogHandler'
 import { Button } from '@components/Button'
 import { Field } from '@components/Field'
 
@@ -13,14 +18,71 @@ import styles from './styles.module.scss'
 export function Form() {
   const {
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
     setFocus,
     register,
+    setError,
   } = useForm<SignInFormType>({
     resolver: zodResolver(signInSchema),
   })
 
-  function signIn(input: SignInFormType) {}
+  const { send } = useSignIn()
+
+  async function signIn(input: SignInFormType) {
+    const { email, password } = input
+
+    try {
+      await send({ email, password })
+    } catch (error) {
+      const requestError = parseError<SignInFormType>(error)
+
+      if (requestError.error.code) {
+        const { code, message, validation } = requestError.error
+
+        if (code === 'validation' && validation) {
+          const { field, message } = validation
+
+          setTimeout(() => {
+            setError(field, { message }, { shouldFocus: true })
+          }, 0)
+        }
+
+        if (code === 'user.not.exists') {
+          setTimeout(() => {
+            setError('email', { message }, { shouldFocus: true })
+          }, 0)
+        }
+
+        if (code === 'password.wrong') {
+          setTimeout(() => {
+            setError('password', { message }, { shouldFocus: true })
+          }, 0)
+        }
+
+        return
+      }
+
+      if (!navigator.onLine) {
+        showAlert({
+          type: 'error',
+          icon: 'ph-wifi-x',
+          title: 'You seem to be offline',
+          description: 'Please check your internet connection and try again.',
+          closeButtonText: 'Ok',
+        })
+        return
+      }
+
+      showAlert({
+        type: 'error',
+        icon: 'ph-warning-circle',
+        title: 'Ooops... Something went wrong!',
+        description:
+          'Your login could not be completed at this time. You can try again at any time.',
+        closeButtonText: 'Close',
+      })
+    }
+  }
 
   useLayoutEffect(() => setFocus('email'), [setFocus])
 
@@ -36,6 +98,7 @@ export function Form() {
         leadingIcon="envelope-simple"
         placeholder="Just type your e-mail bro"
         error={errors.email}
+        disabled={isSubmitting}
         {...register('email')}
       />
 
@@ -45,10 +108,16 @@ export function Form() {
         leadingIcon="lock-simple"
         placeholder="I won't tell anyone 🤫"
         error={errors.password}
+        disabled={isSubmitting}
         {...register('password')}
       />
 
-      <Button type="submit" trailingIcon="arrow-right">
+      <Button
+        type="submit"
+        trailingIcon="arrow-right"
+        disabled={isSubmitting}
+        submiting={isSubmitting}
+      >
         Sign in
       </Button>
     </form>

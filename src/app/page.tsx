716b33113a64d163c5b58dbbc27a7f0f83af1c51
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-06-24 13:49:18 -0300

    feat(HomePage): add components and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,61 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
 
-import commonStyles from '@styles/common.module.scss'
+import styles from './page.module.scss'
 
 export const metadata: Metadata = {
   title: 'Hey user',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    noimageindex: true,
+    indexifembedded: false,
+  },
 }
 
 export default function HomePage() {
   return (
-    <section>
-      <div className={commonStyles.contentContainer}>
-        <h1>Hey user</h1>
+    <section className={styles.page}>
+      <header>
+        <span>Hello, User</span>
+        <h1>My profile</h1>
+      </header>
 
-        <nav>
-          <Link href="/change/name">Change name</Link>
-          <Link href="/change/email">Change email address</Link>
-          <Link href="/change/password">Change password</Link>
-        </nav>
-      </div>
+      <nav>
+        <Link href="/change/name">
+          <i className="ph ph-smiley"></i>
+
+          <div>
+            <strong>Name</strong>
+            <span>John Doe</span>
+          </div>
+
+          <i className="ph ph-pencil-simple"></i>
+        </Link>
+
+        <Link href="/change/email">
+          <i className="ph ph-envelope"></i>
+
+          <div>
+            <strong>E-mail address</strong>
+            <span>john.doe@example.com</span>
+          </div>
+
+          <i className="ph ph-pencil-simple"></i>
+        </Link>
+
+        <Link href="/change/password">
+          <i className="ph ph-key"></i>
+
+          <div>
+            <strong>Password</strong>
+            <span>Change my password</span>
+          </div>
+
+          <i className="ph ph-pencil-simple"></i>
+        </Link>
+      </nav>
     </section>
   )
 }

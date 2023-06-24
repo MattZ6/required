e79094dc78a9792d424cde1b8a54d16f3e5ec859
Matheus Felipe Zanin <matt_z6@hotmail.com>
2023-06-24 14:05:36 -0300

    feat: change email page (#361)

--- src/app/change/email/page.tsx
+++ src/app/change/email/page.tsx
@@ -1,17 +1,41 @@
 import type { Metadata } from 'next'
 
-import commonStyles from '@styles/common.module.scss'
+import { PageHeader } from '@components/PageHeader'
+
+import { Form } from './components'
+import styles from './styles.module.scss'
 
 export const metadata: Metadata = {
-  title: 'Change email address',
+  title: 'Change e-mail address',
+  description:
+    'Change your e-mail address (for future access and communication messages)',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Change e-mail address',
+    description:
+      'Change your e-mail address (for future access and communication messages)',
+    site: 'https://required.vercel.app',
+  },
+  openGraph: {
+    type: 'website',
+    title: 'Change e-mail address',
+    siteName: 'required',
+    description:
+      'Change your e-mail address (for future access and communication messages)',
+    url: '/change/email',
+  },
 }
 
 export default function ChangeEmailPage() {
   return (
-    <section>
-      <div className={commonStyles.contentContainer}>
-        <h1>Change email address</h1>
-      </div>
+    <section className={styles.page}>
+      <PageHeader
+        title="Change e-mail address"
+        description="Change your e-mail address (for future access and communication messages)"
+        backLinkURL="/"
+      />
+
+      <Form />
     </section>
   )
 }
